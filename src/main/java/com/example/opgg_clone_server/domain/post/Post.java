1496c10010ea150f04ee20f224535535538bbb31
so1omon <solmin3665@gmail.com>
2023-01-23 16:13:58 +0900

    [test] test commit and push

--- src/main/java/com/example/opgg_clone_server/domain/post/Post.java
+++ src/main/java/com/example/opgg_clone_server/domain/post/Post.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -12,5 +14,6 @@ import javax.persistence.Id;
 public class Post {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
